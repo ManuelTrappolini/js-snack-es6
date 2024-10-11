f8d602bea39ab6e2e106342c567fc1ffd36ef983
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-11 14:53:50 +0200

    initial commit

---
+++ Snack-1/main.js
@@ -0,0 +1,27 @@
+//Snack 1
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+
+const bikes = [
+    {
+        nome :'Europa',
+        peso : 250,
+    },
+    {
+        nome :'America',
+        peso : 450,
+    },
+    {
+        nome :'Africa',
+        peso : 550,
+    },
+    {
+        nome :'Oceania',
+        peso : 850,
+    },
+
+]
+
+console.log(bikes[0],[1])
