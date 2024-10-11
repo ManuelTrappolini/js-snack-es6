93e678ca4d466e36c58da8c890de6e1568d9bbf5
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-11 16:21:12 +0200

    add for of loop in snack 2

--- Snack-2/main.js
+++ Snack-2/main.js
@@ -4,3 +4,45 @@
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 //Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+let soccerTeam = [
+    { 
+        nome: 'Roma',
+        punti_fatti: 0,
+        falli_subiti:0,
+
+    },
+    { 
+        nome: 'Milan',
+        punti_fatti: 0,
+        falli_subiti: 0,
+
+    },
+    { 
+        nome: 'Inter',
+        punti_fatti: 0,
+        falli_subiti: 0,
+
+    },
+    { 
+        nome: 'Cagliari',
+        punti_fatti: 0,
+        falli_subiti: 0,
+
+    },
+
+]
+ console.log(soccerTeam);
+ 
+
+
+
+
+
+for( let team of soccerTeam)
+    team.punti_fatti = Math.floor(Math.random() *100)
+
+for( let team of soccerTeam)
+    team.falli_subiti = Math.floor(Math.random() *100)
+
+console.log(soccerTeam);
